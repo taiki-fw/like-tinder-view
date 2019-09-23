65837ee4143207541d7b94de728facd0af385543
森　泰樹 <taiki@mbp-2.local>
2019-09-23 18:16:46 +0900

    Personカードのスタイルをstyled-componentsで実装

--- src/People.jsx
+++ src/People.jsx
@@ -10,40 +10,44 @@ export default class People extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      people: {
-        imgPath: model2,
-        name: "Lara",
-        age: "22"
-      }
-      // people: [
-      //   {
-      //     imgPath: model1,
-      //     name: "Lara",
-      //     age: "22"
-      //   },
-      //   {
-      //     imgPath: model2,
-      //     name: "Sandra",
-      //     age: "18"
-      //   },
-      //   {
-      //     imgPath: model3,
-      //     name: "Emma",
-      //     age: "24"
-      //   }
-      // ]
+      people: [
+        {
+          imgPath: model1,
+          name: "Lara",
+          age: "22"
+        },
+        {
+          imgPath: model2,
+          name: "Sandra",
+          age: "18"
+        },
+        {
+          imgPath: model3,
+          name: "Emma",
+          age: "24"
+        }
+      ]
     };
   }
 
   render() {
-    return (
-      <div>
-        <Person
-          imgPath={this.state.people.imgPath}
-          name={this.state.people.name}
-          age={this.state.people.age}
-        />
-      </div>
-    );
+    const item = this.state.people.map((item, index) => (
+      <Person
+        key={index}
+        imgPath={item.imgPath}
+        name={item.name}
+        age={item.age}
+      />
+    ));
+    return <div style={style.People}>{item}</div>;
   }
 }
+
+const style = {
+  People: {
+    position: "relative",
+    zIndex: "1",
+    width: "85%",
+    margin: "0 auto"
+  }
+};
