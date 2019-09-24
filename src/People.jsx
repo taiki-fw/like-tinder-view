07be13efe2805b594a654ac8d9a15731770a22a1
森　泰樹 <taiki@mbp-2.local>
2019-09-24 11:49:55 +0900

    react-animationのためのライブラリの導入と初期設定

--- src/People.jsx
+++ src/People.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group"; // ES6
 
 import Person from "./components/Person";
 
@@ -6,10 +7,29 @@ import model1 from "./img/model-1.jpg";
 import model2 from "./img/model-2.jpg";
 import model3 from "./img/model-3.jpg";
 
+const peopleData = [
+  {
+    imgPath: model1,
+    name: "Lara",
+    age: "22"
+  },
+  {
+    imgPath: model2,
+    name: "Sandra",
+    age: "18"
+  },
+  {
+    imgPath: model3,
+    name: "Emma",
+    age: "24"
+  }
+];
+
 export default class People extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLike: false,
       people: [
         {
           imgPath: model1,
@@ -41,7 +61,18 @@ export default class People extends React.Component {
     ));
     return (
       <>
-        <div style={style.People}>{item}</div>
+        <ReactCSSTransitionGroup
+          transitionName={{
+            enter: "enter",
+            enterActive: "enter--active",
+            leave: "leave",
+            leaveActive: `leave--${this.state.isLike}`
+          }}
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
+          <div style={style.People}>{item}</div>
+        </ReactCSSTransitionGroup>
       </>
     );
   }
